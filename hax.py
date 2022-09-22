3b32de07a0499ec5b15b24ec0c8e624364b310bb
lereti <lereti@users.noreply.github.com>
2022-09-22 11:53:35 +0300

    Added English

--- hax.py
+++ hax.py
@@ -4,10 +4,10 @@ print("while True:")
 while True:
 	print("\t", end = "")
 	if i % 2 == 0:
-		print(f"if i == {i}: \n\t\tprint('Parillinen')")
+		print(f"if i == {i}: \n\t\tprint('Even/Parillinen')")
 		i += 1
 	elif i % 2 != 0:
-		print(f"if i == {i}: \n\t\tprint('Pariton')")
+		print(f"if i == {i}: \n\t\tprint('Odd/Pariton')")
 		i += 1
 	if i == 100000:
 		break
